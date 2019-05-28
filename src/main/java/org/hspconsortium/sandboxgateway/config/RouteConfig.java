74ee545b42d5835ff15db9f825d0e2a18a058888
Jacob Crump <jacob@interopion.com>
2019-05-28 16:04:42 -0600

    Adding config for changing the response body.

--- src/main/java/org/hspconsortium/sandboxgateway/config/RouteConfig.java
+++ src/main/java/org/hspconsortium/sandboxgateway/config/RouteConfig.java
@@ -1,5 +1,7 @@
 package org.hspconsortium.sandboxgateway.config;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import org.hl7.fhir.instance.model.Bundle;
 import org.hspconsortium.sandboxgateway.filter.FhirEndpointResolutionFilter;
 import org.hspconsortium.sandboxgateway.service.FhirEndpointResolutionService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,10 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
 
 @Configuration
 public class RouteConfig {
@@ -19,7 +25,15 @@ public class RouteConfig {
         return builder.routes()
                 .route(p -> p
                         .path("/**")
-                        .filters(f -> f.filter(new FhirEndpointResolutionFilter().apply(new FhirEndpointResolutionFilter.Config(fhirEndpointResolutionService)), 1))
+                        .filters(f -> f.filter(new FhirEndpointResolutionFilter().apply(new FhirEndpointResolutionFilter.Config(fhirEndpointResolutionService)))
+                                .removeRequestHeader("accept-encoding")
+                                .removeRequestHeader("accept")
+                                .addRequestHeader("accept", "application/json")
+                                .modifyResponseBody(String.class, String.class, MediaType.TEXT_PLAIN_VALUE,
+                                        (exchange, bundle) -> {
+                                            bundle = bundle.replaceAll("http://localhost:12100", "googoo");
+                                            return Mono.just(bundle);
+                                        }))
                         .uri("http://httpbin.org:80"))
                 .build();
     }
