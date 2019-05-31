5382cffc295a11d7cbe72ce9d7e1a7cde2ed0188
Shilpy <shilpy@interopion.com>
2019-05-31 15:17:07 -0600

    trying out a change with the CorsConfig

--- src/main/java/org/hspconsortium/sandboxgateway/config/CorsConfig.java
+++ src/main/java/org/hspconsortium/sandboxgateway/config/CorsConfig.java
@@ -14,26 +14,47 @@ import org.springframework.web.server.WebFilterChain;
 
 import reactor.core.publisher.Mono;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Configuration
 public class CorsConfig {
 
-    private static final String ALLOWED_HEADERS = "X-FHIR-Starter, authorization, Prefer, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers";
-    private static final String ALLOWED_METHODS = "GET, PUT, POST, DELETE, OPTIONS, PATCH";
+    private static final List<String> ALLOWED_HEADERS = new ArrayList<>();
+    private static final List<HttpMethod> ALLOWED_METHODS = new ArrayList<>();
     private static final String ALLOWED_ORIGIN = "*";
-    private static final String MAX_AGE = "3600";
+    private static final long MAX_AGE = 3600;
 
     @Bean
     public WebFilter corsFilter() {
+
+        ALLOWED_METHODS.add(HttpMethod.GET);
+        ALLOWED_METHODS.add(HttpMethod.PUT);
+        ALLOWED_METHODS.add(HttpMethod.POST);
+        ALLOWED_METHODS.add(HttpMethod.DELETE);
+        ALLOWED_METHODS.add(HttpMethod.OPTIONS);
+        ALLOWED_METHODS.add(HttpMethod.PATCH);
+
+        ALLOWED_HEADERS.add("X-FHIR-Starter");
+        ALLOWED_HEADERS.add("authorization");
+        ALLOWED_HEADERS.add("Prefer");
+        ALLOWED_HEADERS.add("Origin");
+        ALLOWED_HEADERS.add("Accept");
+        ALLOWED_HEADERS.add("X-Requested-With");
+        ALLOWED_HEADERS.add("Content-Type");
+        ALLOWED_HEADERS.add("Access-Control-Request-Method");
+        ALLOWED_HEADERS.add("Access-Control-Request-Headers");
+
         return (ServerWebExchange ctx, WebFilterChain chain) -> {
             ServerHttpRequest request = ctx.getRequest();
             if (CorsUtils.isCorsRequest(request)) {
                 ServerHttpResponse response = ctx.getResponse();
                 HttpHeaders headers = response.getHeaders();
-                headers.add("Access-Control-Allow-Origin", ALLOWED_ORIGIN);
-                headers.add("Access-Control-Allow-Methods", ALLOWED_METHODS);
-                headers.add("Access-Control-Max-Age", MAX_AGE);
-                headers.add("Access-Control-Allow-Headers",ALLOWED_HEADERS);
+                headers.setAccessControlAllowOrigin(ALLOWED_ORIGIN);
+                headers.setAccessControlAllowMethods(ALLOWED_METHODS);
+                headers.setAccessControlMaxAge(MAX_AGE);
+                headers.setAccessControlAllowHeaders(ALLOWED_HEADERS);
                 if (request.getMethod() == HttpMethod.OPTIONS) {
                     response.setStatusCode(HttpStatus.OK);
                     return Mono.empty();
