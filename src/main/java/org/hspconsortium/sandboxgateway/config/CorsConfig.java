adc2eea566c7d9f629b4197995986d455ba2deb7
Shilpy <shilpy@interopion.com>
2019-06-03 09:50:08 -0600

    cors issue

--- src/main/java/org/hspconsortium/sandboxgateway/config/CorsConfig.java
+++ src/main/java/org/hspconsortium/sandboxgateway/config/CorsConfig.java
@@ -1,10 +1,15 @@
 package org.hspconsortium.sandboxgateway.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
+import java.util.Arrays;
 
 
 @Configuration
@@ -18,7 +23,25 @@ public class CorsConfig implements WebFluxConfigurer {
                 .allowedMethods("*")
                 .maxAge(3600);
     }
+
+    @Bean
+    CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Arrays.asList("*"));
+        corsConfig.setMaxAge(8000L);
+        corsConfig.addAllowedMethod("*");
+        corsConfig.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
+    }
 }
+
+
+
 
 //import org.springframework.context.annotation.Bean;
 //import org.springframework.http.HttpHeaders;
