0a4d6fecc4301b360f917b8d50c5ddcb0d4bb489
Shilpy <shilpy@interopion.com>
2019-06-03 09:32:48 -0600

    cors issue

--- src/main/java/org/hspconsortium/sandboxgateway/config/CorsConfig.java
+++ src/main/java/org/hspconsortium/sandboxgateway/config/CorsConfig.java
@@ -1,52 +1,70 @@
 package org.hspconsortium.sandboxgateway.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.http.server.reactive.ServerHttpResponse;
-import org.springframework.web.cors.reactive.CorsUtils;
-import org.springframework.web.server.ServerWebExchange;
-import org.springframework.web.server.WebFilter;
-import org.springframework.web.server.WebFilterChain;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
 
-import reactor.core.publisher.Mono;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 @Configuration
-public class CorsConfig {
+@EnableWebFlux
+public class CorsConfig implements WebFluxConfigurer {
 
-    private static final String ALLOWED_HEADERS = "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN";
-    private static final String ALLOWED_METHODS = "GET, PUT, POST, DELETE, OPTIONS";
-    private static final String ALLOWED_ORIGIN = "*";
-    private static final String MAX_AGE = "3600";
-
-    @Bean
-    public WebFilter corsFilter() {
-        return (ServerWebExchange ctx, WebFilterChain chain) -> {
-            ServerHttpRequest request = ctx.getRequest();
-            if (CorsUtils.isCorsRequest(request)) {
-                ServerHttpResponse response = ctx.getResponse();
-                HttpHeaders headers = response.getHeaders();
-                headers.add("Access-Control-Allow-Origin", ALLOWED_ORIGIN);
-                headers.add("Access-Control-Allow-Methods", ALLOWED_METHODS);
-                headers.add("Access-Control-Max-Age", MAX_AGE);
-                headers.add("Access-Control-Allow-Headers",ALLOWED_HEADERS);
-                if (request.getMethod() == HttpMethod.OPTIONS) {
-                    response.setStatusCode(HttpStatus.OK);
-                    return Mono.empty();
-                }
-            }
-            return chain.filter(ctx);
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .maxAge(3600);
     }
-
 }
+
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.http.HttpHeaders;
+//        import org.springframework.http.HttpMethod;
+//        import org.springframework.http.HttpStatus;
+//        import org.springframework.http.server.reactive.ServerHttpRequest;
+//        import org.springframework.http.server.reactive.ServerHttpResponse;
+//        import org.springframework.web.cors.reactive.CorsUtils;
+//import org.springframework.web.server.ServerWebExchange;
+//        import org.springframework.web.server.WebFilter;
+//        import org.springframework.web.server.WebFilterChain;
+//
+//        import reactor.core.publisher.Mono;
+//
+//        import java.util.ArrayList;
+//        import java.util.List;
+
+//@Configuration
+//public class CorsConfig {
+//
+//    private static final String ALLOWED_HEADERS = "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN";
+//    private static final String ALLOWED_METHODS = "GET, PUT, POST, DELETE, OPTIONS";
+//    private static final String ALLOWED_ORIGIN = "*";
+//    private static final String MAX_AGE = "3600";
+//
+//    @Bean
+//    public WebFilter corsFilter() {
+//        return (ServerWebExchange ctx, WebFilterChain chain) -> {
+//            ServerHttpRequest request = ctx.getRequest();
+//            if (CorsUtils.isCorsRequest(request)) {
+//                ServerHttpResponse response = ctx.getResponse();
+//                HttpHeaders headers = response.getHeaders();
+//                headers.add("Access-Control-Allow-Origin", ALLOWED_ORIGIN);
+//                headers.add("Access-Control-Allow-Methods", ALLOWED_METHODS);
+//                headers.add("Access-Control-Max-Age", MAX_AGE);
+//                headers.add("Access-Control-Allow-Headers",ALLOWED_HEADERS);
+//                if (request.getMethod() == HttpMethod.OPTIONS) {
+//                    response.setStatusCode(HttpStatus.OK);
+//                    return Mono.empty();
+//                }
+//            }
+//            return chain.filter(ctx);
+//        };
+//    }
+//
+//}
 
 //    private static final List<String> ALLOWED_HEADERS = new ArrayList<>();
 //    private static final List<HttpMethod> ALLOWED_METHODS = new ArrayList<>();
